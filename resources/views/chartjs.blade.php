d5db9c2acfed3c57cb65f772042fc3ce05b636ea
librankaio <rafiqzayana@gmail.com>
2022-12-20 09:13:55 +0700

    update chart

--- resources/views/chartjs.blade.php
+++ resources/views/chartjs.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <section class="section">
     <div class="section-header">
-      <h1>Chart.JS</h1>
+      <h1>Welcome, {{ session('comp_name') }}</h1>
       <div class="section-header-breadcrumb">
         <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
         <div class="breadcrumb-item"><a href="#">Modules</a></div>
